2a9af323b59567fbcc5ef3cbd1780bd4cf77c7d7
klynn726 <49536087+klynn726@users.noreply.github.com>
2021-04-10 11:25:49 -0700

    bug fix

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-
+let badge = "";
 //https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
 const renderLicenseBadge = (license) => {
   if (license == 'None') {
